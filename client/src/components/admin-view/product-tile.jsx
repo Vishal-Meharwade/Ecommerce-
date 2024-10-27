061bd4586bdb55267772149f5362d784ae0fe0a3
Vishal <vishalmeharwade1@gmail.com>
2024-10-27 19:30:55 +0530

    layout change

--- client/src/components/admin-view/product-tile.jsx
+++ client/src/components/admin-view/product-tile.jsx
@@ -29,7 +29,7 @@ function AdminProductTile({
               ₹{product?.price}
             </span>
             {product?.salePrice > 0 ? (
-              <span className="text-lg font-bold">${product?.salePrice}</span>
+              <span className="text-lg font-bold">₹{product?.salePrice}</span>
             ) : null}
           </div>
         </CardContent>
